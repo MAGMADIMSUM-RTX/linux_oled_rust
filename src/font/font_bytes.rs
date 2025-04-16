0e6417e46f3006bbc3297e153c3e070550c38896
MAGMADIMSUM-RTX <madmaliu@bupt.edu.cn>
2025-04-16 17:39:37 +0800

    重构OLED模块，删除未使用的代码，更新版本至0.400.0

--- src/font/font_bytes.rs
+++ src/font/font_bytes.rs
@@ -1,126 +1,7 @@
-// use core::panic;
-use std::ptr;
 
-struct ASCIIFont {
-    h: u8,
-    w: u8,
-    chars: &'static [u8],
-}
+pub const NONE_CHAR: [char; 0] = [];
 
-pub struct Font {
-    h: u8,
-    w: u8,
-    chars: &'static [char],
-    ascii: ASCIIFont,
-}
-// 24x24
-impl Font {
-    pub fn get_char(&self, ch: char) -> Option<Vec<Vec<u8>>> {
-        // if !ch.is_ascii() || ch < ' ' || ch > 'z' {
-        //     return None;
-        // }
-        let (char_data, rows, cols) = if ch.is_ascii() && ch < ' ' {
-            return None;
-        } else if ch.is_ascii() {
-            let index = (ch as u8 - ' ' as u8) as usize;
-            let len: usize = ((self.ascii.h / 8) * self.ascii.w) as usize;
-            let char_data = &self.ascii.chars[index * len..(index + 1) * len];
-            // 确定行数（高度除以8，因为每个字节代表8个垂直像素）
-            let rows = (self.ascii.h / 8) as usize;
-            let cols = self.ascii.w as usize;
-            (char_data, rows, cols)
-        } else {
-            // 处理非ASCII字符
-            // if let Some(index) = self.chars.iter().position(|&c| c == ch) {
-            //     let len = ((self.h / 8) * self.w) as usize;
-            //     let char_data = self.chars[index * len..(index + 1) * len]
-            //         .iter()
-            //         .map(|&c| c as u8)
-            //         .collect::<Vec<u8>>()
-            //         .as_slice();
-            //     let rows = (self.h / 8) as usize;
-            //     let cols = self.w as usize;
-            //     (char_data, rows, cols)
-            println!("Unsupported character: {}", ch);
-            // } else {
-            return None;
-            // }
-        };
-
-        // 创建二维数组
-        let mut result: Vec<Vec<u8>> = Vec::with_capacity(rows);
-        for row in 0..rows {
-            let mut row_data: Vec<u8> = Vec::with_capacity(cols);
-            for col in 0..cols {
-                row_data.push(char_data[row * cols + col]);
-            }
-            result.push(row_data);
-        }
-        Some(result)
-    }
-
-    pub fn get_char_width(&self, ch: char) -> Option<u8> {
-        if ch.is_ascii() {
-            if ch < ' ' {
-                return None;
-            }
-            return Some(self.ascii.w);
-        } else {
-            if self.chars.iter().any(|&c| c == ch) {
-                return Some(self.w);
-            }
-            return None;
-        }
-    }
-
-    pub fn get_font_height(&self) -> u8 {
-        self.h
-    }
-}
-
-struct Image {
-    w: u8,
-    h: u8,
-    data: &'static [u8],
-}
-
-const AFONT8X6: ASCIIFont = ASCIIFont {
-    h: 8,
-    w: 6,
-    chars: unsafe {
-        ptr::slice_from_raw_parts(ASCII_8X6.as_ptr() as *const u8, 92 * 6)
-            .as_ref()
-            .unwrap()
-    },
-};
-
-pub const FONT8X8: Font = Font {
-    h: 8,
-    w: 8,
-    chars: &NONE_CHAR,
-    ascii: AFONT8X6,
-};
-
-const AFONT24X12: ASCIIFont = ASCIIFont {
-    h: 24,
-    w: 12,
-    chars: unsafe {
-        ptr::slice_from_raw_parts(ASCII_24X12.as_ptr() as *const u8, 95 * 36)
-            .as_ref()
-            .unwrap()
-    },
-};
-
-pub const FONT24X24: Font = Font {
-    h: 24,
-    w: 24,
-    chars: &NONE_CHAR,
-    ascii: AFONT24X12,
-};
-
-const NONE_CHAR: [char; 0] = [];
-
-const ASCII_8X6: [[u8; 6]; 92] = [
+pub const ASCII_8X6: [[u8; 6]; 92] = [
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00], // space 空格
     [0x00, 0x00, 0x00, 0x2f, 0x00, 0x00], // !
     [0x00, 0x00, 0x07, 0x00, 0x07, 0x00], // "
@@ -215,7 +96,7 @@ const ASCII_8X6: [[u8; 6]; 92] = [
     [0x14, 0x14, 0x14, 0x14, 0x14, 0x14], // horiz lines
 ];
 
-const ASCII_24X12: [[u8; 36]; 95] = [
+pub const ASCII_24X12: [[u8; 36]; 95] = [
     [
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
---
+++ src/font/mod.rs
@@ -0,0 +1,126 @@
+// use core::panic;
+use std::ptr;
+mod font_bytes;
+use font_bytes::{ASCII_8X6, ASCII_24X12, NONE_CHAR};
+
+struct ASCIIFont {
+    h: u8,
+    w: u8,
+    chars: &'static [u8],
+}
+
+pub struct Font {
+    h: u8,
+    w: u8,
+    chars: &'static [char],
+    ascii: ASCIIFont,
+}
+
+pub struct Buffer<'a> {
+    pub data: Vec<&'a str>,
+    pub len: usize,
+}
+
+impl Font {
+    pub fn get_char(&self, ch: char) -> Option<Vec<Vec<u8>>> {
+        // if !ch.is_ascii() || ch < ' ' || ch > 'z' {
+        //     return None;
+        // }
+        let (char_data, rows, cols) = if ch.is_ascii() && ch < ' ' {
+            return None;
+        } else if ch.is_ascii() {
+            let index = (ch as u8 - ' ' as u8) as usize;
+            let len: usize = ((self.ascii.h / 8) * self.ascii.w) as usize;
+            let char_data = &self.ascii.chars[index * len..(index + 1) * len];
+            // 确定行数（高度除以8，因为每个字节代表8个垂直像素）
+            let rows = (self.ascii.h / 8) as usize;
+            let cols = self.ascii.w as usize;
+            (char_data, rows, cols)
+        } else {
+            // 处理非ASCII字符
+            // if let Some(index) = self.chars.iter().position(|&c| c == ch) {
+            //     let len = ((self.h / 8) * self.w) as usize;
+            //     let char_data = self.chars[index * len..(index + 1) * len]
+            //         .iter()
+            //         .map(|&c| c as u8)
+            //         .collect::<Vec<u8>>()
+            //         .as_slice();
+            //     let rows = (self.h / 8) as usize;
+            //     let cols = self.w as usize;
+            //     (char_data, rows, cols)
+            println!("Unsupported character: {}", ch);
+            // } else {
+            return None;
+            // }
+        };
+
+        // 创建二维数组
+        let mut result: Vec<Vec<u8>> = Vec::with_capacity(rows);
+        for row in 0..rows {
+            let mut row_data: Vec<u8> = Vec::with_capacity(cols);
+            for col in 0..cols {
+                row_data.push(char_data[row * cols + col]);
+            }
+            result.push(row_data);
+        }
+        Some(result)
+    }
+
+    pub fn get_char_width(&self, ch: char) -> Option<u8> {
+        if ch.is_ascii() {
+            if ch < ' ' {
+                return None;
+            }
+            return Some(self.ascii.w);
+        } else {
+            if self.chars.iter().any(|&c| c == ch) {
+                return Some(self.w);
+            }
+            return None;
+        }
+    }
+
+    pub fn get_font_height(&self) -> u8 {
+        self.h
+    }
+}
+
+struct Image {
+    w: u8,
+    h: u8,
+    data: &'static [u8],
+}
+
+const AFONT8X6: ASCIIFont = ASCIIFont {
+    h: 8,
+    w: 6,
+    chars: unsafe {
+        ptr::slice_from_raw_parts(ASCII_8X6.as_ptr() as *const u8, 92 * 6)
+            .as_ref()
+            .unwrap()
+    },
+};
+
+pub const FONT8X8: Font = Font {
+    h: 8,
+    w: 8,
+    chars: &NONE_CHAR,
+    ascii: AFONT8X6,
+};
+
+const AFONT24X12: ASCIIFont = ASCIIFont {
+    h: 24,
+    w: 12,
+    chars: unsafe {
+        ptr::slice_from_raw_parts(ASCII_24X12.as_ptr() as *const u8, 95 * 36)
+            .as_ref()
+            .unwrap()
+    },
+};
+
+pub const FONT24X24: Font = Font {
+    h: 24,
+    w: 24,
+    chars: &NONE_CHAR,
+    ascii: AFONT24X12,
+};
